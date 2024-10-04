3355c7bccc1ce9e6b09105f4908463c4f9158b73
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-10-04 20:18:33 -0300

    Random passwords generator

--- JwtStore.Core/AccountContext/ValueObjects/Password.cs
+++ JwtStore.Core/AccountContext/ValueObjects/Password.cs
@@ -4,9 +4,24 @@ namespace JwtStore.Core.AccountContext.ValueObjects;
 
 public class Password : ValueObject {
     
-    public string Valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    public string Special = "!@#$%^&*{}[](),.;:-_+-/";
+    public static string Valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    public static string Special = "!@#$%^&*{}[](),.;:-_+-/";
 
-    public string Hash { get; } = string.Empty;
+    public string Hash { get; } = GeneratePassword();
     public string ResetCode { get; } = Guid.NewGuid().ToString("N")[..8].ToUpper();
+
+    private static string GeneratePassword(short lenght = 16, bool includeSpecial = true,
+        
+        bool upperCase = false) {
+        var chars = includeSpecial ? (Valid + Special) : Valid;
+        int start = upperCase ? 26 : 0;
+        int index = 0;
+        var result = new char[lenght];
+        var random = new Random();  
+
+        while(index < lenght)
+            result[index++] = chars[random.Next(start, chars.Length)];
+
+        return new string(result);
+    }
 }
