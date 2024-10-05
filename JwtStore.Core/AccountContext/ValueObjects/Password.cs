35c218c79d463f0069cd78edfca3ef8ce3cfe7c9
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-10-04 21:06:18 -0300

    Password hashing

--- JwtStore.Core/AccountContext/ValueObjects/Password.cs
+++ JwtStore.Core/AccountContext/ValueObjects/Password.cs
@@ -1,4 +1,5 @@
 ﻿using JwtStore.Core.SharedContext.ValueObjects;
+using System.Security.Cryptography;
 
 namespace JwtStore.Core.AccountContext.ValueObjects;
 
@@ -11,9 +12,9 @@ public class Password : ValueObject {
     public string ResetCode { get; } = Guid.NewGuid().ToString("N")[..8].ToUpper();
 
     private static string GeneratePassword(short lenght = 16, bool includeSpecial = true,
-        
         bool upperCase = false) {
-        var chars = includeSpecial ? (Valid + Special) : Valid;
+
+        string chars = includeSpecial ? (Valid + Special) : Valid;
         int start = upperCase ? 26 : 0;
         int index = 0;
         var result = new char[lenght];
@@ -23,5 +24,21 @@ public class Password : ValueObject {
             result[index++] = chars[random.Next(start, chars.Length)];
 
         return new string(result);
+    }
+
+    private static string Hashing(string password, short saltSize = 16,
+        short keySize = 32, int iterations = 10000, char splitChar = '.') {
+        
+        if (string.IsNullOrEmpty(password))
+            throw new Exception("Password shouldnt be empty!");
+
+        password += Configuration.Secrets.PasswordSaltKey;
+
+        using Rfc2898DeriveBytes algorithm = new(password, saltSize, 
+            iterations, HashAlgorithmName.SHA256);
+        string key = Convert.ToBase64String(algorithm.GetBytes(keySize));
+        string salt = Convert.ToBase64String(algorithm.Salt);
+
+        return $"{iterations}{splitChar}{salt}{splitChar}{key}";
     }
 }
