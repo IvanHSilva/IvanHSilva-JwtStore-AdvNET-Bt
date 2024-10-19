32f8275678b4843a6e8ae572812d47f869de33c7
IvanHSilva <ivanhenriquessilva@gmail.com>
2024-10-19 16:17:34 -0300

    Configuration class

--- JwtStore.Core/Configuration.cs
+++ JwtStore.Core/Configuration.cs
@@ -4,7 +4,11 @@ public static class Configuration {
 
     public static SecretsConfiguration Secrets { get; set; } = new();
 
-    public class SecretsConfiguration {
+    public class Database {
+        public static string ConnectionString { get; set; } = string.Empty;
+    }
+
+   public class SecretsConfiguration {
         public string ApiKey { get; set; } = string.Empty;
         public string JwtPrivateKey { get; set; } = string.Empty;
         public string PasswordSaltKey { get; set; } = string.Empty;
