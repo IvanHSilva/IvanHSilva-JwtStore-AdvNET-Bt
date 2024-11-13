8f64469f0d30c9ac5959d5933a5040c6890e2c0d
IvanHS <ivanhenriquessilva@gmail.com>
2024-11-13 16:54:11 -0300

    Repository implementation

---
+++ JwtStore.Infra/Contexts/AccountContext/UseCases/Insert/Repository.cs
@@ -0,0 +1,21 @@
+﻿using JwtStore.Core.Contexts.AccountContext.Entities;
+using JwtStore.Core.Contexts.AccountContext.UseCases.Contracts;
+using JwtStore.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtStore.Infra.Contexts.AccountContext.UseCases.Insert;
+
+public class Repository : IRepository {
+
+    private readonly AppDbContext _context;
+
+    public Repository(AppDbContext context) => _context = context;
+
+    public async Task<bool> AnyAsync(string email, CancellationToken cancellationToken)
+        => await _context.Users.AsNoTracking().AnyAsync(u => u.Email == email, cancellationToken);
+
+    public async Task SaveAsync(User user, CancellationToken cancellationToken) {
+        await _context.Users.AddAsync(user, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ JwtStore.Infra/Contexts/AccountContext/UseCases/Insert/Service.cs
@@ -0,0 +1,10 @@
+﻿using JwtStore.Core.Contexts.AccountContext.Entities;
+using JwtStore.Core.Contexts.AccountContext.UseCases.Contracts;
+
+namespace JwtStore.Infra.Contexts.AccountContext.UseCases.Insert;
+
+public class Service : IService {
+    public Task SendVerificationEmailAsync(User user, CancellationToken cancellationToken) {
+        throw new NotImplementedException();
+    }
+}
