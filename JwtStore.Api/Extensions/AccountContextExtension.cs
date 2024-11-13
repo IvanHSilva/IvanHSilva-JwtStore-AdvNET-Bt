00af5bf271fff70188e9926da7cec3f5265de5f0
IvanHS <ivanhenriquessilva@gmail.com>
2024-11-13 18:17:43 -0300

    API configuration

---
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -0,0 +1,7 @@
+﻿namespace JwtStore.Api.Extensions; 
+
+public static class AccountContextExtension {
+    public static void AddAccountContext(this WebApplicationBuilder builder){}
+
+    public static void MapAccountEndpoints(this WebApplication app) { }
+}
